7566637e88f1fee1e5fecfb57aa017fb1c98e987
Braden Everson <bradeneverson@gmail.com>
2024-10-02 10:51:14 -0500

    Accept websocket connections and forward them to main

--- fps-server/src/main.rs
+++ fps-server/src/main.rs
@@ -7,7 +7,8 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let (mut engine, sender) = SessionEngine::<usize>::new();
+    let (mut engine, _game_sender) = SessionEngine::<usize>::new();
+    let (sender, mut receiver) = tokio::sync::mpsc::channel(100);
     let listener = TcpListener::bind("0.0.0.0:7878")
         .await
         .expect("Failed to start up server");
@@ -28,6 +29,12 @@ async fn main() {
                     tracing::error!("Error serving connection :( {}", e)
                 }
             });
+        }
+    });
+
+    tokio::spawn(async move {
+        while let Some(_stream) = receiver.recv().await {
+            // New client stream, add it to in lobby clients
         }
     });
 
--- fps-server/src/server.rs
+++ fps-server/src/server.rs
@@ -6,20 +6,40 @@ use std::pin::Pin;
 use http_body_util::Full;
 use hyper::body::Bytes;
 use hyper::service::Service;
+use hyper::upgrade::Upgraded;
 use hyper::{body, Request, Response};
+use hyper_util::rt::TokioIo;
 use tokio::sync::mpsc::Sender;
-
-use crate::engine::GameInfo;
+use tokio_tungstenite::WebSocketStream;
 
 /// Everything necessary to add a new game session to the engine
-pub struct GameManager(pub Sender<GameInfo<usize>>);
+pub struct GameManager(pub Sender<ClientStream>);
+
+/// A Client WebSocket read and write stream
+pub type ClientStream = WebSocketStream<TokioIo<Upgraded>>;
 
 impl Service<Request<body::Incoming>> for GameManager {
     type Response = Response<Full<Bytes>>;
     type Error = hyper::http::Error;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
-    fn call(&self, _req: Request<body::Incoming>) -> Self::Future {
-        todo!()
+    fn call(&self, mut req: Request<body::Incoming>) -> Self::Future {
+        let sender = self.0.clone();
+        if hyper_tungstenite::is_upgrade_request(&req) {
+            let (response, websocket) = hyper_tungstenite::upgrade(&mut req, None).expect("Error upgrading to websocket");
+
+            tokio::spawn(async move {
+                match websocket.await {
+                    Ok(ws) => {
+                        sender.send(ws).await.expect("Failed to transfer client websocket connection");
+                    }
+                    Err(e) => tracing::error!("Failed to construct websocket, {}", e)
+                }
+            });
+
+            Box::pin(async { Ok(response) })
+        } else {
+            todo!()
+        }
     }
 }
